27a155368e08b061aac8815122d2a320c8338e2f
Ahmed Gamal <ahmedmgh67@gmail.com>
2019-03-30 19:17:18 +0200

    git unignored build and ignored maps

---
+++ build/precache-manifest.9ed269fb553d8ca768d0e560070cae7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "938359070023af7b36ea",
+    "url": "/static/js/main.a5914406.chunk.js"
+  },
+  {
+    "revision": "a624e006150e2275c8e5",
+    "url": "/static/js/2.b039c840.chunk.js"
+  },
+  {
+    "revision": "938359070023af7b36ea",
+    "url": "/static/css/main.a2046809.chunk.css"
+  },
+  {
+    "revision": "cc46a90f6d0145e7e32a4fc4f566a19d",
+    "url": "/index.html"
+  }
+];
--- src/OrderItem.js
+++ src/OrderItem.js
@@ -1,26 +1,6 @@
 import * as m from '@material-ui/core';
 import React from 'react';
-import { Link }from 'react-router-dom';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
-
-function OrderIteam  (props)  {
-  return (
-    <div>
-      <m.ListItem size="large" divider>  
-        <m.ListItemText primary={props.address} secondary={props.price}></m.ListItemText>
-        <Link 
-          class="link" 
-          exact 
-          to="/orders/details"
-            ><m.Button 
-            variant="outlined"
-            >Details</m.Button>
-        </Link>
-      </m.ListItem>
-    </div>
-  );
-};
-
 
 export default function OrderItem (props) {
 
